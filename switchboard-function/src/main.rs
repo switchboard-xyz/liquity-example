cb60f8bd5a1b8dcf70fba6d48577633c23ca20ae
Mitch Gildenberg <mitch@switchboard.xyz>
2023-09-01 06:51:15 -0400

    solidity compiles

--- switchboard-function/src/main.rs
+++ switchboard-function/src/main.rs
@@ -1,25 +1,30 @@
 pub mod pyth;
 pub use pyth::*;
+pub mod kraken;
+pub use kraken::*;
 
-use ethers::types::Bytes;
-use base64;
+
 use chrono::Utc;
+
+
 use ethers::{
     prelude::{abigen, SignerMiddleware},
     providers::{Http, Provider},
     types::Address,
 };
-use rust_decimal::prelude::FromPrimitive;
-use rust_decimal::Decimal;
+
+use hex;
+
+
 use serde::Deserialize;
 use switchboard_evm::sdk::EVMFunctionRunner;
 pub use switchboard_utils::reqwest;
-use ethers::prelude::Wallet;
-use ethers_core::rand::thread_rng;
 
-
-abigen!(Receiver, r#"[ function callback(address[], bytes[]) ]"#,);
-static UNUSED_URL: &str = "https://goerli-rollup.arbitrum.io/rpc";
+abigen!(
+    Receiver,
+    r#"[ function callback(uint256[], address[], bytes32[], bytes[]) ]"#,
+);
+static DEFAULT_URL: &str = "https://goerli-rollup.arbitrum.io/rpc";
 
 #[derive(Debug, Deserialize)]
 pub struct DeribitRespnseInner {
@@ -36,37 +41,119 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --- Initialize clients ---
     let function_runner = EVMFunctionRunner::new()?;
     let receiver: Address = env!("EXAMPLE_PROGRAM").parse()?;
-    let provider = Provider::<Http>::try_from(UNUSED_URL)?;
+    let provider = Provider::<Http>::try_from(DEFAULT_URL)?;
     let signer = function_runner.enclave_wallet.clone();
     let client = SignerMiddleware::new_with_provider_chain(provider, signer).await?;
     let receiver_contract = Receiver::new(receiver, client.into());
 
-    // // --- Send the callback to the contract with Switchboard verification ---
-    // let callback = receiver_contract.callback(0.into(), 0.into());
-    // let expiration = (Utc::now().timestamp() + 120).into();
-    // let gas_limit = 5_500_000.into();
-    // function_runner.emit(receiver, expiration, gas_limit, vec![callback])?;
+    // --- Logic Below ---
+    // DERIVE CUSTOM SWITCHBOARD PRICE
+    let kraken_url = "https://api.kraken.com/0/public/Ticker?pair=BTCUSD";
+    let kraken_spot: KrakenTickerResponse = reqwest::get(kraken_url).await?.json().await?;
+    let (_, kraken_btc_usd) = kraken_spot.result.iter().next().unwrap();
+    let mut kraken_price = kraken_btc_usd.price();
+    kraken_price.rescale(8);
+    let switchboard_prices = vec![
+        kraken_price.mantissa().into(),
+        kraken_price.mantissa().into(),
+    ];
+
+    // CHAINLINK
+    // https://docs.chain.link/data-feeds/price-feeds/addresses/?network=arbitrum
+    let chainlink_price_ids = vec![
+        "0xA39434A63A52E749F02807ae27335515BA4b07F7".parse()?, // BTC/USD
+        "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e".parse()?, // ETH/USD
+    ];
+
+    // PYTH
+    // https://pyth.network/developers/price-feed-ids#pyth-evm-testnet
+    let pyth_price_ids = vec![
+        "0xf9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b", // BTC/USD
+        "0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6", // ETH/USD
+    ];
+    let pyth_vaas = pyth::fetch_testnet_vaas(pyth_price_ids.clone()).await?;
+    let _fee = pyth::fetch_fee_for_vaas(&pyth_vaas).await?;
+    let pyth_price_ids: Vec<[u8; 32]> = pyth_price_ids
+        .iter()
+        .map(|x| hex::decode(&x[2..]).unwrap().try_into().unwrap())
+        .collect();
+    // --- END LOGIC ---
+
+    println!(
+        "{}, {:#?}, {:?}, {:?}",
+        kraken_price.clone(),
+        chainlink_price_ids.clone(),
+        pyth_price_ids.clone(),
+        pyth_vaas.clone()
+    );
+
+    // --- Send the callback to the contract with Switchboard verification ---
+    let callback = receiver_contract.callback(
+        switchboard_prices,
+        chainlink_price_ids,
+        pyth_price_ids,
+        pyth_vaas,
+    );
+    let expiration = (Utc::now().timestamp() + 120).into();
+    let gas_limit = 5_500_000.into();
+    function_runner.emit(receiver, expiration, gas_limit, vec![callback])?;
     Ok(())
 }
 
 /// Run `cargo test -- --nocapture`
 #[cfg(test)]
 mod tests {
-    use crate::*;
     use crate::pyth::*;
-    use ethers::prelude::LocalWallet;
+    use crate::*;
+    
 
     #[tokio::test]
-    async fn test() {
+    async fn test() -> Result<(), Box<dyn std::error::Error>> {
+        let kraken_url = "https://api.kraken.com/0/public/Ticker?pair=BTCUSD";
+        let kraken_spot: KrakenTickerResponse = reqwest::get(kraken_url).await?.json().await?;
+        let (_, kraken_btc_usd) = kraken_spot.result.iter().next().unwrap();
+        let mut kraken_price = kraken_btc_usd.price();
+        kraken_price.rescale(8);
+        let switchboard_prices = vec![
+            kraken_price.mantissa().into(),
+            kraken_price.mantissa().into(),
+        ];
         // https://docs.chain.link/data-feeds/price-feeds/addresses/?network=arbitrum
-        let chainlink_btc_usd = "0xA39434A63A52E749F02807ae27335515BA4b07F7";
-        let chainlink_eth_usd = "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e";
+        let chainlink_price_ids = vec![
+            "0xA39434A63A52E749F02807ae27335515BA4b07F7".parse()?, // BTC/USD
+            "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e".parse()?, // ETH/USD
+        ];
         // https://pyth.network/developers/price-feed-ids#pyth-evm-testnet
-        let vaas = pyth::fetch_testnet_vaas(vec![
+        let pyth_price_ids = vec![
             "0xf9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b", // BTC/USD
             "0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6", // ETH/USD
-        ]).await.unwrap();
-        let fee = pyth::fetch_fee_for_vaas(&vaas).await.unwrap();
-        println!("{:#?} -- {}", vaas, fee);
+        ];
+        let pyth_vaas = pyth::fetch_testnet_vaas(pyth_price_ids.clone()).await?;
+        let _fee = pyth::fetch_fee_for_vaas(&pyth_vaas).await?;
+        let pyth_price_ids: Vec<[u8; 32]> = pyth_price_ids
+            .iter()
+            .map(|x| hex::decode(&x[2..]).unwrap().try_into().unwrap())
+            .collect();
+        println!(
+            "{}, {:#?}, {:?}, {:?}",
+            kraken_price.clone(),
+            chainlink_price_ids.clone(),
+            pyth_price_ids.clone(),
+            pyth_vaas.clone()
+        );
+
+        let function_runner = EVMFunctionRunner::new()?;
+        let receiver: Address = env!("EXAMPLE_PROGRAM").parse()?;
+        let provider = Provider::<Http>::try_from(DEFAULT_URL)?;
+        let signer = function_runner.enclave_wallet.clone();
+        let client = SignerMiddleware::new_with_provider_chain(provider, signer).await?;
+        let receiver_contract = Receiver::new(receiver, client.into());
+        let _callback = receiver_contract.callback(
+            switchboard_prices,
+            chainlink_price_ids,
+            pyth_price_ids,
+            pyth_vaas,
+        );
+        Ok(())
     }
 }
--- switchboard-function/src/pyth/mod.rs
+++ switchboard-function/src/pyth/mod.rs
@@ -1,26 +1,26 @@
 use base64;
-pub use switchboard_utils::reqwest;
-use ethers::types::Bytes;
 use ethers::abi::Address;
-use ethers::types::U256;
 use ethers::prelude::*;
- use rand::thread_rng;
+use ethers::types::Bytes;
+use ethers::types::U256;
+use rand::thread_rng;
+pub use switchboard_utils::reqwest;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-
 static ARBITRUM_TESTNET_URL: &str = "https://goerli-rollup.arbitrum.io/rpc";
-    // https://docs.pyth.network/documentation/pythnet-price-feeds/evm#networks
+// https://docs.pyth.network/documentation/pythnet-price-feeds/evm#networks
 static PYTH_ADDRESS_ARBITRUM_TESTNET: &str = "0x939C0e902FF5B3F7BA666Cc8F6aC75EE76d3f900";
-abigen!(PythContract, r#"[ function getUpdateFee(bytes[]) view returns (uint256) ]"#,);
+abigen!(
+    PythContract,
+    r#"[ function getUpdateFee(bytes[]) view returns (uint256) ]"#,
+);
 
 pub fn b64_to_hex(input: &str) -> Result<String> {
     let decoded_bytes = base64::decode(input)?;
 
     // Convert bytes to hexadecimal string
-    let hex_string: String = decoded_bytes.iter()
-        .map(|b| format!("{:02x}", b))
-        .collect();
+    let hex_string: String = decoded_bytes.iter().map(|b| format!("{:02x}", b)).collect();
 
     // Prepend 0x
     Ok(format!("0x{}", hex_string))
@@ -28,8 +28,16 @@ pub fn b64_to_hex(input: &str) -> Result<String> {
 
 pub async fn fetch_testnet_vaas(addresses: Vec<&str>) -> Result<Vec<Bytes>> {
     let addresses = addresses.join("&ids[]=");
-    let pyth_vaas: Vec<Bytes> = reqwest::get(format!("https://xc-testnet.pyth.network/api/latest_vaas?ids[]={}", addresses))
-        .await?.json::<Vec<String>>().await?.iter().map(|x| base64::decode(x).unwrap().into()).collect();
+    let pyth_vaas: Vec<Bytes> = reqwest::get(format!(
+        "https://xc-testnet.pyth.network/api/latest_vaas?ids[]={}",
+        addresses
+    ))
+    .await?
+    .json::<Vec<String>>()
+    .await?
+    .iter()
+    .map(|x| base64::decode(x).unwrap().into())
+    .collect();
     Ok(pyth_vaas)
 }
 
